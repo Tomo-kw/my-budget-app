929f4e72fbf9fc0c0dc49dda84455772aca32db3
Tomo-kw <t.kuwahara@miraito-inc.co.jp>
2023-07-22 15:42:49 +0900

    Prettierの設定及び適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Button, ChakraProvider } from '@chakra-ui/react';
+import React from 'react'
+import logo from './logo.svg'
+import './App.css'
+import { Box, Button, ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
     <ChakraProvider>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <Button>ChakraButton</Button>
-        </header>
-      </div>
+      <Box>my-budget-app作る！</Box>
     </ChakraProvider>
-  );
+  )
 }
 
-export default App;
+export default App
