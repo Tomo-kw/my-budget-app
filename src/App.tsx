bca13597340ffcb7d847b1c3d10839f8e068756e
Tomo-kw <t.kuwahara@miraito-inc.co.jp>
2023-07-22 22:03:44 +0900

    ESLintの導入

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import React from 'react'
-import logo from './logo.svg'
-import './App.css'
-import { Box, Button, ChakraProvider } from '@chakra-ui/react'
+import { Box, ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
     <ChakraProvider>
-      <Box>my-budget-app作る！</Box>
+      <Box>my-budget-app作る</Box>
     </ChakraProvider>
   )
 }
